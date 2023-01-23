b6cb09e93f46856267f34dec04cd7d1928c1ec2a
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-01-23 15:20:54 -0500

    Draw two colorful triangles to the screen

--- GPR300_EmptyWindow/GPR300_EmptyWindow/main.cpp
+++ GPR300_EmptyWindow/GPR300_EmptyWindow/main.cpp
@@ -8,25 +8,33 @@ void resizeFrameBufferCallback(GLFWwindow* window, int width, int height);
 
 //TODO: Vertex shader source code
 const char* vertexShaderSource =
-"#version 450								\n"
-"layout (location = 0) in vec3 vPos;		\n"
-"void main() {								\n"
-"	gl_Position = vec4(vPos, 1.0);			\n"
-"}											\0";
+"#version 450							\n"
+"layout (location = 0) in vec3 vPos;	\n"
+"layout (location = 1) in vec3 vColor;	\n"
+"out vec3 Color;						\n"
+"void main() {							\n"
+"	Color = vColor;						\n"
+"	gl_Position = vec4(vPos, 1.0);		\n"
+"}										\0";
 
 //TODO: Fragment shader source code
 const char* fragmentShaderSource =
-"#version 450								\n"
-"out vec4 FragColor;						\n"
-"void main() {								\n"
-"	FragColor = vec4(1.0, 1.0, 1.0, 1.0);	\n"
-"}											\0";
+"#version 450							\n"
+"out vec4 FragColor;					\n"
+"in vec3 Color;							\n"
+"void main() {							\n"
+"	FragColor = vec4(Color, 1.0);		\n"
+"}										\0";
 
 //TODO: Vertex data array
 const float vertexData[] = { 
-	-0.5, -0.5, 0.0,
-	0.5, -0.5, 0.0,
-	0.0, 0.5, 0.0
+	// x  y     z    r    g    b    a
+	-0.5, 0.5,  0.0, 1.0, 0.0, 0.0, 1.0,
+	-0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0,
+	0.0,  0.0,  0.0, 0.0, 0.0, 1.0, 1.0,
+	0.5,  0.5,  0.0, 1.0, 0.0, 0.0, 1.0,
+	0.0,  0.0,  0.0, 0.0, 1.0, 0.0, 1.0,
+	0.5,  -0.5, 0.0, 0.0, 0.0, 1.0, 1.0
 };
 
 int main() {
@@ -105,9 +113,13 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
-	//TODO: Define vertex attribute layout
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	//TODO: Define vertex attribute layout (position (xyz))
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
 	glEnableVertexAttribArray(0);
+
+	// Define vertex attribute layout (color (rgba))
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.6f, 1.0f);
@@ -116,8 +128,8 @@ int main() {
 		//TODO:Use shader program
 		glUseProgram(shaderProgram);
 		
-		//TODO: Draw triangle (3 indices!)
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//TODO: Draw triangle (6 indices!)
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
