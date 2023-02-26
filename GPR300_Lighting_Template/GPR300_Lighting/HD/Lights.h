8deaf5d37f772a411894093028bb8c54025ee4e0
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-02-26 17:52:34 -0500

    Added draw GUI function for spot light

--- GPR300_Lighting_Template/GPR300_Lighting/HD/Lights.h
+++ GPR300_Lighting_Template/GPR300_Lighting/HD/Lights.h
@@ -5,18 +5,6 @@
 #include "glm/glm.hpp"
 #include <string>
 
-#include "../EW/Transform.h"
-#include "glm/glm.hpp"
-
-struct PointLight {
-	ew::Transform transform;
-	glm::vec3 color;
-	float intensity;
-	float ambientLevel;
-	float minRadius;
-	float maxRadius;
-};
-
 struct DirectionalLight {
 	ew::Transform transform;
 	glm::vec3 color;
@@ -41,14 +29,23 @@ struct DirectionalLight {
 	}
 };
 
+struct PointLight {
+	ew::Transform transform;
+	glm::vec3 color;
+	float intensity;
+	float ambientLevel;
+	float minRadius;
+	float maxRadius;
+};
+
 struct SpotLight {
 	ew::Transform transform;
 	glm::vec3 color;
 	float intensity;
 	float ambientLevel;
-	float angularAttenuation;
 	float minAngle;
 	float maxAngle;
+	std::string name;
 
 	glm::vec3 direction() {
 		float x = cos(glm::radians(transform.rotation.y)) * cos(glm::radians(transform.rotation.x));
@@ -56,5 +53,15 @@ struct SpotLight {
 		float z = sin(glm::radians(transform.rotation.y)) * cos(glm::radians(transform.rotation.x));
 
 		return glm::normalize(glm::vec3(x, y, z));
+	}
+
+	void drawGui() {
+		ImGui::ColorEdit3((name + " Color").c_str(), &color.x);
+		ImGui::DragFloat3((name + " Position").c_str(), &transform.position.x);
+		ImGui::DragFloat2((name + " Rotation").c_str(), &transform.rotation.x);
+		ImGui::SliderFloat((name + " Intensity").c_str(), &intensity, 0.0, 1.0);
+		ImGui::SliderFloat((name + " Ambient Level").c_str(), &ambientLevel, 0.0, 1.0);
+		ImGui::DragFloat((name + " Min Angle").c_str(), &minAngle);
+		ImGui::DragFloat((name + " Max Angle").c_str(), &maxAngle);
 	}
 };
